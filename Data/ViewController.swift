c0a9335a759cc75a7453236a2824fef71d8af7da
许正荣 <842841022@qq.com>
2017-05-22 11:46:47 +0800

    Initial Commit

---
+++ Data/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Data
+//
+//  Created by 许正荣 on 2017/5/22.
+//  Copyright © 2017年 许正荣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

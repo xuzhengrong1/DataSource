9047ec239091609a3060f0713fc90058b9870643
许正荣 <842841022@qq.com>
2017-05-22 15:43:03 +0800

    runtime

--- Data/ViewController.swift
+++ Data/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+
 class ViewController: UITableViewController {
     
     var dataSource : GeneralDataSource<UITableViewCell, Person>?
@@ -48,5 +50,26 @@ class ViewController: UITableViewController {
     }
 
 
+}
+
+
+extension UIViewController{
+    open  override static func initialize()
+    {
+//        methodSwizThreadSafe(self, #selector(UIViewController.viewWillAppear(_:)), #selector(UIViewController.xl_viewWillAppear(animated:)))
+        
+         methodSwizThreadSafe(self, #selector(UIViewController.viewDidLoad), #selector(UIViewController.xl_viewDidLoad))
+    }
+    
+    func xl_viewWillAppear(animated: Bool) {
+        self.xl_viewWillAppear(animated: animated)
+        print("xl_viewWillAppear in swizzleMethod")
+    }
+    
+
+    func xl_viewDidLoad() {
+        self.xl_viewDidLoad()
+        print("xl_viewDidLoad in swizzleMethod")
+    }
 }
 
